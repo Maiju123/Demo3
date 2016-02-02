329552feeb801930fbbd831b81e4e7addb388dda
Maiju Schreck <K2322@student.jamk.fi>
2016-02-02 13:46:14 +0200

    kotitehtava

---
+++ Demo3_Kotitehtava4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Kotitehtava4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vechile vechile = new Vechile();
+            vechile.PrintData();
+
+            Console.WriteLine("Vechile= " + vechile.Name);
+
+            Console.WriteLine(vechile.ToString());
+        }
+    }
+}
---
+++ Demo3_Kotitehtava4/Vechile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Kotitehtava4
+{
+    class Vechile
+    {
+        // properties
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+
+        // methods
+
+            // constructions
+        public Vechile()
+        { 
+            Speed = 100;
+            Tyres = 4;  
+        }
+       // public Vechile(string Name)
+        //{
+          //  Name = Auto;
+        //}  
+        public void PrintData()
+        {
+            Console.WriteLine("Vechile Name is: {0}", Name);
+            Console.WriteLine("Vechile can go: {0} km/h", Speed);
+            Console.WriteLine("Vechile has {0} tyres", Tyres);
+        }
+        //returns object properties in a one string
+        public override string ToString()
+        {
+            return "Name=  " + Name + " Speed= " + Speed + " Tyres= " + Tyres;
+        }
+    }
+}
